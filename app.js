3b1ca5de91cd6e9feeed84c35cfb0c24d0888c99
Facundo Lobo <facundolobo2@gmail.com>
2021-07-08 21:28:37 -0300

    despliegue 08-07-2021

--- app.js
+++ app.js
@@ -6,43 +6,37 @@ const hbs = require('hbs');
 const app = express()
 const port = process.env.PORT;
 
-
 //Handlebars
+app.set('view engine','hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
-//TODO require('hbs')
-app.set('view engine','hbs');
 
 //Servir contenido estático
 app.use(express.static('public')); //cuando ingresa '/' redigira al index.html
 
 
-app.get('/',  (req, res) => {
-    res.render('home',{
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    }); //lo enviamos al archvo credo en public
-})
-
-app.get('/generic', (req, res) => {
-    res.render('generic',{
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    }); //lo enviamos al archvo credo en public
-})
 
 // app.get('/generic', (req, res) => {
-//     res.sendFile(__dirname+'/public/generic.html'); //lo enviamos al archvo credo en public
+//     res.render('generic',{
+//         nombre: 'Fernando Herrera',
+//         titulo: 'Curso de Node'
+//     }); //lo enviamos al archvo credo en public
 // })
 
-app.get('/elements', (req, res) => {
-    res.render('elements',{
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    }); //lo enviamos al archvo credo en public
-})
+// // app.get('/generic', (req, res) => {
+// //     res.sendFile(__dirname+'/public/generic.html'); //lo enviamos al archvo credo en public
+// // })
 
+// app.get('/elements', (req, res) => {
+//     res.render('elements',{
+//         nombre: 'Fernando Herrera',
+//         titulo: 'Curso de Node'
+//     }); //lo enviamos al archvo credo en public
+// })
 
+app.get('*', (req, res) => {
+        res.sendFile(__dirname+'/public/index.html'); //lo enviamos al archvo credo en public
+    })
 
 
 
